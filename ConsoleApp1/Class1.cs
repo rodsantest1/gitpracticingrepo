c99ec1da41544ca704fdec8a514b852110640b2b
Rodney Santiago <rodsan.724@outlook.com>
2020-12-03 18:22:02 -0500

    Adding AddNumbers service to Class1

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     public class Class1
     {
+        public static int AddNumbers(string input1, string input2)
+        {
+            int.TryParse(input1, out int number1);
+            int.TryParse(input2, out int number2);
+
+            return number1 + number2;
+        }
     }
 }
