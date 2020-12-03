28180ee27ef37a8f693a3b02f067ba9d81783dcc
Rodney Santiago <rodsan.724@outlook.com>
2020-12-03 18:10:03 -0500

    m2 - mvp add 1+2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Console.WriteLine($"1+2={1+2}");
         }
     }
 }
